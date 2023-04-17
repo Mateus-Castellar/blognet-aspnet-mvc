1640f79592a174fe337186b4db197a564aa363a4
Mateus Castellar <mateuscastellar@outlook.com>
2023-04-16 22:10:47 -0300

    feat: Extendo tabela identiy e adicionando campo de foto de perfil de usuarios

--- src/BlogNet.Web/Data/ApplicationDbContext.cs
+++ src/BlogNet.Web/Data/ApplicationDbContext.cs
@@ -1,10 +1,35 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogNet.Web.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(builder);
+    }
+}
+
+public class ApplicationUser : IdentityUser
+{
+    public string? FotoPerfil { get; set; }
+}
+
+public class ApplicationUserMapping : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.ToTable("Usuarios");
+
+        builder.Property(lbda => lbda.FotoPerfil)
+            .HasColumnType("varchar(255)")
+            .IsRequired(false);
+    }
 }
